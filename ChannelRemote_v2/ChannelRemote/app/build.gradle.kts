a9f5f153f4899da5a6d796697e71fcdde40fe409
dornmant01s <dornmant01s@gmail.com>
2025-09-10 21:17:07 +0900

    Install Android SDK in CI

--- ChannelRemote_v2/ChannelRemote/app/build.gradle.kts
+++ ChannelRemote_v2/ChannelRemote/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.channelremote"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.channelremote"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
